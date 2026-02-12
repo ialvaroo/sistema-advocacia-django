f983b91afb50e775e28c5b6b25b656533b935637
Diego Fonseca <167884513+Di0Fonseca@users.noreply.github.com>
2026-02-11 21:19:10 -0300

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('documentos/', views.lista_documentos, name='lista_documentos'), 
     path('selecao/<int:cliente_id>/', views.selecionar_modelo, name='selecionar_modelo'),
     path('gerar_doc/<int:cliente_id>/<int:modelo_id>/', views.gerar_documento, name='gerar_documento'),
+    path('editar/<int:id>/', views.editar_cliente, name='editar_cliente'),
 ]
